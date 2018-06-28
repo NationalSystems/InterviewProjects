5f50b9821e7c22be6e7050c2ef880c17c15fcb5d
Christian Brandalise <cbrandalise@quikorder.com>
2018-06-28 12:46:57 -0500

    FE Interview

---
+++ front_end/react_hoc/src/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+import axios from 'axios';
+
+const MyComponent = ({posts}) => {
+};
+
+const HOC = (PassedComponent) => {
+};
+
+
+const App = () => {
+    return (
+        <div>
+            <p>Hello, World</p>
+        </div>
+    );
+}
+
+render(<App/>, document.getElementById('index'))
+
