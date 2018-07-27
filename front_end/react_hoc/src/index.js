c3b35e29b137871356745496a24f52e4c905397d
Christian Brandalise <cbrandalise@quikorder.com>
2018-07-27 09:47:56 -0500

    better boilerplate for react_hoc

--- front_end/react_hoc/src/index.js
+++ front_end/react_hoc/src/index.js
@@ -2,17 +2,34 @@ import React, {Component} from 'react';
 import { render } from 'react-dom';
 import axios from 'axios';
 
+class PostService {
+    static get posts() {
+        return axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.data); 
+    }
+}
+
 const MyComponent = ({posts}) => {
+    return <div>Howdy! 😃</div>;
 };
 
-const HOC = (PassedComponent) => {
-};
+const withData = (WrappedComponent) => {
+    return class extends Component {
+        constructor() {
+            super();
+        }
 
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
+    }
+};
 
 const App = () => {
+    const Posts = withData(MyComponent)
     return (
         <div>
-            <p>Hello, World</p>
+            <Posts/>
         </div>
     );
 }
